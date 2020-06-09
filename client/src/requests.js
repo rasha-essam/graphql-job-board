3eae4ae6b70cefdb46942e8e07c7ca9108f5b43b
rasha-essam <engrasha88@gmail.com>
2020-06-09 13:57:49 +0200

    Returning Jobs for a Company

--- client/src/requests.js
+++ client/src/requests.js
@@ -54,6 +54,10 @@ export async function loadCompany(id) {
       id
       name
       description
+      jobs{
+          id
+          title
+      }
     }
   }`;
   const { company } = await graphqlRequest(query, { id });
