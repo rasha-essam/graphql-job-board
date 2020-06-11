bd127ce0b43c23d4a2d559c39bd8ed411497ed5e
rasha-essam <engrasha88@gmail.com>
2020-06-11 16:41:03 +0200

    Caching and Fetch Policy

--- client/src/requests.js
+++ client/src/requests.js
@@ -39,7 +39,7 @@ export async function loadJobs() {
   `;
   const {
     data: { jobs }
-  } = await client.query({ query });
+  } = await client.query({ query, fetchPolicy: "no-cache" });
 
   return jobs;
 }
